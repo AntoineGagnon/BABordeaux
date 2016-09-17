a0866f45197ec138125dd2b2769f1f88efcdfea7
unknown <antoine.gagnon@etu.u-bordeaux.fr>
2016-09-17 13:31:02 +0200

    added database connection info

---
+++ resources/views/sondage_creator.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- New Sondage Form -->
+        <form action="/sondage" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- Nom du sondage -->
+            <div class="form-group">
+                <label for="sondage" class="col-sm-3 control-label">Sondage</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="sondage-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- Add Sondage Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Sondage
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- TODO: Current Sondages -->
+@endsection
