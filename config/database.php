6ad91ece8c51a0ba4ab6c02c5ca06cf183945365
unknown <antoine.gagnon@etu.u-bordeaux.fr>
2016-09-17 13:31:16 +0200

    forgot files on last commit *

--- config/database.php
+++ config/database.php
@@ -55,9 +55,10 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
+            'database'  => env('DB_DATABASE', 'GoogSondage'),
+            'username'  => env('DB_USERNAME', ''),
             'password'  => env('DB_PASSWORD', ''),
+            'port'      => env('DB_PORT','80'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
