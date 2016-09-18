46bc85198d102003a8a3eb37d4dbbf2e27ae1914
Pc bureau val <sniper33470@hotmail.fr>
2016-09-18 12:32:00 +0200

    Sondage Creation /  add choices

--- resources/views/sondage_creator.blade.php
+++ resources/views/sondage_creator.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
+<script type="text/javascript">
+    var ordre =0;
+    var choice =0;
+    function addQuestion() {
+        var questions = document.getElementById('questions');
+        questions.innerHTML ="";
+    }
+    function addChoice(){
+        var choices = document.getElementById('choices');
+        choices.innerHTML += '<input type="text" name="'+choice+'" class="form-control" placeholder="choice">';
+    }
+</script>
+
 <!-- Bootstrap Boilerplate... -->
 <div class="panel panel-default">
     <div class="panel-heading">
@@ -24,17 +37,40 @@
                 </div>
             </div>
 
+            <!-- Questions -->
+            <div id="questions">
+                
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <input type="text" name="label" class="form-control" placeholder="Label" >
+                    </div>
+
+                    <div class="panel-body" id="choices">
+                        <button type="button" class="btn btn-default" onclick="addChoice();">Add Choice</button>
+                    </div>
+                </div>
+            
+            </div>
+
+            <!-- Add Question Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="button" class="btn btn-default" onclick="addQuestion();">Add Question</button>
+                </div>
+            </div>
+
             <!-- Add Sondage Button -->
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Sondage
+                    <button type="submit" class="btn btn-primary btn-lg">
+                        <i class="fa fa-plus">Add Sondage</i>
                     </button>
                 </div>
             </div>
         </form>
     </div>
 </div>
+
 
 <!-- TODO: Current Sondages -->
 @endsection
