53777161ded8e71326701b12316611d8da6a8945
Antoine Gagnon <antoine.gagnon@etu.u-bordeaux.fr>
2016-11-06 17:47:37 +0100

    Changed db config

---
+++ app/Http/Controllers/PollController.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Sondage;
+use App\Question;
+use App\Choix;
+use App\Reponse;
+use App\Http\Requests;
+
+class PollController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //This is used to make sure the name is filled and < 255 char
+        $validator = Validator::make($request->all(), [
+            'titre' => 'required|max:255',
+            'mdp' => 'required|max:60',
+            'q_label1' => 'required',
+            'choice1_1' => 'required_with:q_label1',
+            'choice1_2' => 'required_with:q_label1',
+            'choice2_1' => 'required_with:q_label2',
+            'choice2_2' => 'required_with:q_label2',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $sondage = new Sondage;
+        $sondage->titre = $request->titre;
+        $sondage->mdp = Hash::make($request->mdp);
+        $sondage->save();
+        $question1 = new Question;
+        $question1->label = $request->q_label1;
+        $question1->Sondage_Id =$sondage->id;
+        $question1->ordre = 1;
+        $question1->save();
+        $choix1_1 = new Choix;
+        $choix1_1->Question_Id = $question1->id;
+        $choix1_1->label = $request->choice1_1;
+        $choix1_1->save();
+        $choix1_2 = new Choix;
+        $choix1_2->Question_Id = $question1->id;
+        $choix1_2->label = $request->choice1_2;
+        $choix1_2->save();
+        if(!empty($request->q_label2)){
+            $question2 = new Question;
+            $question2->Sondage_Id =$sondage->id;
+            $question2->ordre = 2;
+            $question2->label = $request->q_label2;
+            $question2->save();
+            $choix2_1 = new Choix;
+            $choix2_1->Question_Id = $question2->id;
+            $choix2_1->label = $request->choice2_1;
+            $choix2_1->save();
+            $choix2_2 = new Choix;
+            $choix2_2->Question_Id = $question2->id;
+            $choix2_2->label = $request->choice2_2;
+            $choix2_2->save();
+        }
+        echo "Sondage avec le titre $request->titre sauvegardé !";
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $questions = Question::all();
+        $answers = array();
+        foreach($questions as $question){
+            $question['answers']=Answer::where('Question_id','=',$question->id)->orderBy('id','asc')->get();
+        }
+
+        //var_dump($choices);
+        return view('poll_view',['questions' => $questions]);
+    }
+
+}
---
+++ resources/views/poll_view.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h1>Sondage du Musée des Beaux Arts</h1>
+        </div>
+
+        <div class="panel-body">
+            <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- Formulaire de création de sondage -->
+            <form action="/poll" method="POST" class="form-horizontal">
+                {{ csrf_field() }}
+                @foreach ($questions as $question)
+
+                    <div class="form-group">
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3>{{ $question->label }}</h3>
+                            </div>
+
+                            <div class="panel-body">
+
+                                @foreach ($question->answers as $answer)
+                                    <INPUT class="radio-inline" type="radio" name="{{ $question->id }}" value= "{{ $answer->label }}" > {{ $choice->label }}
+                                    <br>
+                                @endforeach
+                            </div>
+                        </div>
+
+                    </div>
+
+                @endforeach
+                <button type="submit" class="btn btn-primary pull-right btn-lg" >Valider</button>
+
+            </form>
+        </div>
+    </div>
+
+@endsection
