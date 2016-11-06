c8d885d9e4893c1f526c3dddb24985817f1222fc
Antoine Gagnon <antoine.gagnon@etu.u-bordeaux.fr>
2016-11-06 17:46:17 +0100

    Added poll controller and index function and view

---
+++ app/question.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class question extends Model
+{
+
+    // No need to know when it was created/updated
+    public $timestamps = false;
+}
