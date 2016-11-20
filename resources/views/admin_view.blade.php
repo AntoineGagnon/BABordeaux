e991ca502694060366266570f13007e51ce873d5
Cyril <cyril.niobe@gmail.com>
2016-11-20 15:40:17 +0100

    edit location buttons on bootstrap grid

--- resources/views/admin_view.blade.php
+++ resources/views/admin_view.blade.php
@@ -11,9 +11,9 @@
         <div class="panel-body">
             <!-- Display Validation Errors -->
             @include('common.errors')
-            <div class="btn-group-vertical btn-group-lg " role="group" aria-label="...">
+            <div class="btn-group-vertical btn-group-lg col-lg-12 col-xs-12 col-md12 col-sm-12" role="group" aria-label="...">
                 <a href="/admin/editpoll" type="button" class="btn btn-default btn-danger btn-block">Editer le questionnaire</a>
-                <a href="/admin/resultpoll" type="button" class="btn btn-default btn-info btn-block" >Visualiser des résultats du questionnaire</a>
+                <a href="/admin/resultpoll" type="button" class="btn btn-default btn-info btn-block" >Visualiser les résultats du questionnaire</a>
                 <a href="" type="button" class="btn btn-default btn-success btn-block">Exporter les résultats du questionnaire</a>
                 <a href="/admin/resultguestbook" type="button" class="btn btn-default btn-warning btn-block">Visualiser le Livre d'or</a>
 
