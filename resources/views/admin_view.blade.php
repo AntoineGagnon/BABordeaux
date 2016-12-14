d41bff3414f8ff67c99059cee1d2db01fa0b65b3
Cyril <cyril.niobe@gmail.com>
2016-12-14 14:10:59 +0100

    button export results disabled

--- resources/views/admin_view.blade.php
+++ resources/views/admin_view.blade.php
@@ -23,8 +23,8 @@
                     Livre d'or (Excel)</a>
                 <a href="/admin/exportguestbookresults/pdf" type="button" class="btn btn-default btn-success btn-block">Exporter les résultats du
                     Livre d'or (PDF)</a>
-                <a href="" type="button" class="btn btn-default btn-success btn-block">Exporter les résultats du
-                    questionnaire</a>
+                <!--a href="" type="button" class="btn btn-default btn-success btn-block">Exporter les résultats du
+                    questionnaire</a>-->
                 <a href="/admin/change_password" type="button" class="btn btn-default btn-warning btn-block">Modifier le mot de passe d'administration</a>
                 <a href="/logout" type="button" class="btn btn-default btn-warning btn-block">Déconnexion</a>
             </div>
