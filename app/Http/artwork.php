6ac1a477505fa290ae96ddeeb616c6b0be5be209
Cyril <cyril.niobe@gmail.com>
2017-03-08 02:07:18 +0100

    Added artwork Model class

---
+++ app/Http/artwork.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class artwork extends Model
+{
+    // No need to know when it was created/updated
+    public $timestamps = false;
+
+}
