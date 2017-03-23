fb7327a0b947b52ce090368a8b943d9ca71ccf97
Antoine Gagnon <antoine.gagnon@etu.u-bordeaux.fr>
2017-03-23 13:44:16 +0100

    Ajout de règles supplémentaires possible

--- resources/views/regexp_view.blade.php
+++ resources/views/regexp_view.blade.php
@@ -19,9 +19,13 @@
 
             @else
 
+                <button class="btn btn-primary" id="btnAddRule">Add rule</button>
+
                 <form action="/regexp/search" id="mainForm" method="POST">
                     {{ csrf_field() }}
-                        <button class="btn btn-primary" id="btnAddRule">Add rule</button>
+
+                    <div id="rules">
+
 
 
                     <div class="row" id="row0">
@@ -53,7 +57,7 @@
                         </div>
 
                     </div>
-
+</div>
 
                     <button class="btn btn-primary" type="submit">Search</button>
                 </form>
@@ -68,17 +72,48 @@
     @parent
     <script>
 
+    currentQuestion = 0;
+    currentRule = 0;
+
         $(document).ready(function () {
 
-            $("#rule0").change(function () {
-                console.log($("#rule0").val());
-                if($("#rule0").val() == "between"){
-                    $("#values0").append("<input type='text' class='form-control col-md-6' id='value_greater0' name='value_greater0' placeholder='Max' required/>");
+            $("#rule" + currentRule).change(function () {
+                console.log($("#rule" + currentRule).val());
+                if($("#rule" + currentRule).val() == "between"){
+                    $("#values" + currentRule).append("<input type='text' class='form-control col-md-6' id='value_greater" + currentRule + "' name='value_greater0" + currentRule + "' placeholder='Max' required/>");
 
                 }
             })
             $("#btnAddRule").click(function () {
-                console.log($("#rule0").val());
+              currentRule++;
+                var largeHTML = '<div class="row" id="row' + currentQuestion + "-" +  currentRule + '">' +
+                    '<div class="form-group col-md-2 "> ' +
+                        '<select class="form-control attributeSelector" id="attribute' + currentQuestion + "-" +  currentRule + '" name="attribute' + currentQuestion + "-" +  currentRule + '"> ' +
+                            '@foreach($attributes as $attribute) ' +
+                                '<option value="{{$attribute}}">{{$attribute}}</option> ' +
+                            '@endforeach ' +
+                        '</select> ' +
+                    '</div> ' +
+                    '<div class="form-group  col-md-2">' +
+                        '<select class="form-control ruleSelector" id="rule' + currentQuestion + "-" +  currentRule + '" name="rule' + currentQuestion + "-" +  currentRule + '" title="Règle"> ' +
+                            '<option value="contains">Contient</option> ' +
+                            '<option value="notcontains">Ne contient pas</option> ' +
+                            '<option value="begins">Commence par</option> ' +
+                            '<option value="ends">Finit par</option> ' +
+                            '<option value="between">Entre nombres</option> ' +
+                            '<option value="morethan">Supérieur à</option> ' +
+                            '<option value="lessthan">Inférieur à</option> ' +
+                            '<option value="equalto">Egal à</option> ' +
+                        '</select> ' +
+                    '</div> ' +
+                    '<div class="form-group  col-md-8" > ' +
+                        '<div class="row" id="values' + currentQuestion + "-" +  currentRule + '"> ' +
+                        '<input type="text" class="form-control col-md-6" id="value' + currentQuestion + "-" +  currentRule + '" name="value' + currentQuestion + "-" +  currentRule + '" placeholder="Valeur"  required/>' +
+                        '</div> ' +
+                    '</div> ' +
+                '</div> ';
+
+                $("#rules").append(largeHTML)
 
             })
         })
